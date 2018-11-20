a98c4252f1cec04e54aea2ae35170450d91a1d74
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-11-20 18:55:18 -0300

    continue decoder

--- src/SignalDecoding.jl
+++ src/SignalDecoding.jl
@@ -10,15 +10,38 @@ function main(wav_path, win_size)
 
 	e = s .^ 2
 
-	N = win_size
+	# The samples per period
+	# The longest period is for the largest freq
+	# In this case: 697 Hz ~ 500 Hz
+	samples_period = Integer(0.005 * fs)
+
+	# Window size is 3 periods
+	N = samples_period * 3
 
 	h = ones(N) ./ N
 
 	y = conv(h, e)
 
+	# Keep the samples beginning in N/2
+	# because the convolution shift it in that amount
+	y1 = y[div(N, 2):end]
+
 	y_axis = (0:(length(e) + length(h) - 1)) ./ fs
 
-	p = plot(y_axis, y, title="Signal Energy Windowed (Win: $N)",
+	# Threshold
+	thres = ones(length(y1)) .* 0.0125
+
+	# Detecting tones and silences
+	plot_pulses = map(x -> Int(x), y1 .> thres) .* 0.025
+	pulses = y1 .> thres
+
+	# Indexes where samples are greater than threshold
+	whe = findall(pulses)
+
+	# How long the pulses are (how many consecutive ones)
+	differ = diff(whe)
+
+	p = plot(y_axis, [e, y1, thres, pulses], title="Signal Energy Windowed (Win: $N)",
 	     xlabel="Time [s]", lengend=false)
 
 	savefig(p, "window_$N.png")
